7793a73f127751fc3cf6ae0deac0098e6fa9d357
aponoma <siteandstock@gmail.com>
2024-02-03 15:11:54 +0100

    input title in card dynamically

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,4 +10,6 @@ import { CommonModule } from '@angular/common';
 })
 export class CourseCardComponent {
 
+  @Input()
+  title: string;
 }
