0e0c22b1cad88471da7b742278d47db085e4df56
aponoma <siteandstock@gmail.com>
2024-02-08 17:09:16 +0100

    Angular's built-in pipes.  apply formatting to the 'price' variable by setting the format to the number pipe in the app.component.html to '3.3-5', where the first 3 numbers of integer digits and from 3 to 5 of fractional digits. change the price value and get it rounded on 5th fractional digit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
 
   title = COURSES[1].description;
   startDate = new Date(2000, 0, 1);
-  price = 9.99;
+  price = 9.99242842;
 
   onCourseSelected(course: Course) {
     console.log('App component - click event bubbled ...', course);
