b20fee6c0a06a4ec279ad427930d35e03b819071
aponoma <siteandstock@gmail.com>
2024-02-08 16:44:28 +0100

    Angular's built-in pipes. Add a start date to AppComponent and display it in HTML  using Angular's default date pipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ import {Course} from './model/course';
 export class AppComponent {
   courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
+
   onCourseSelected(course: Course) {
     console.log('App component - click event bubbled ...', course);
   }
