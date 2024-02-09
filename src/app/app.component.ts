2f5f8f149e52c4372dff221e866dbe74e7fea9a8
aponoma <siteandstock@gmail.com>
2024-02-09 18:52:54 +0100

    The View Children Decorator and Query List In Detail: subscribe to changes in cards (first mentioning of the subscription) to log cards to the console on any changes, and initiate such changes by pushing extra course in the array of courses on a button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import {log} from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
 import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
 import {CourseCardComponent} from './course-card/course-card.component';
@@ -17,9 +18,24 @@ export class AppComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    console.log('These cards first element: ', this.cards.first);
+    this.cards.changes.subscribe(
+      cards => console.log(cards)
+    );
   }
 
   onCourseSelected(course: Course) {
+  }
+
+  onCoursesEdited() {
+    this.courses.push(
+      {
+        id: 1,
+        description: 'Angular core deep dive',
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        longDescription: 'A detailed walk-through of the most important part of Angular - the Core and Common modules',
+        category: 'INTERMEDIATE',
+        lessonsCount: 10
+      }
+    );
   }
 }
